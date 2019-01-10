0cdf063f195976ca55f44daea01ba9b5defc05fc
Chenyunhan <453407794@qq.com>
2019-01-10 11:46:59 +0800

    1.10上午

--- js/js_login.js
+++ js/js_login.js
@@ -1,6 +1,6 @@
 $("#d2_b1").click(function () {
     var name = $("#name").val();
-
+    alert(name);
     $.ajax({
         url: "http://192.168.9.196:7300/mock/5c1c376e48ca380e48e47bae/loginByPwd",
         type: "POST",
@@ -10,6 +10,7 @@ $("#d2_b1").click(function () {
                 $.cookie('name', name, {
                     expires: 1,
                 })
+                alert($.cookie("name"));
                 window.location.href = "./mine.html";
             }
         },
