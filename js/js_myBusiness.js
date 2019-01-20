496265786d7dd94e264e0c3029d833a04d902200
Chenyunhan <453407794@qq.com>
2019-01-20 08:58:37 +0800

    1.19

--- js/js_myBusiness.js
+++ js/js_myBusiness.js
@@ -18,9 +18,9 @@ $.ajax({
                 } else {
                     qylx = 'u102';
                 }
-                cpy.append('<li><div class="media col-xs-12 i4_1"><div class="media-left"><img src="./img/u10.png" class="media-object" alt="企业" style="height: 17px"></div><div class="media-body" style="width: 100%;"><p style="font-size: 16px">' + dwdz_mc + '</p><p style="color:rgba(0, 0, 0, 0.5);margin-bottom: 5px;">' + unit_mc + '</p><p style="color:rgba(0, 0, 0, 0.5)">' + fwqx + '</p></div><div class="media-right"><img src="./img/' + qylx + '.png" alt="小规模"></div></div></li><li><div class="col-xs-12" style="height: 9px"></div></li>');
-
+                cpy.append('<li><div class="media col-xs-12 i4_1"><div class="media-left"><img src="./img/u10.png" class="media-object" alt="企业" style="height: 17px"></div><div class="media-body" style="width: 100%;"><p class="p1">' + dwdz_mc + '</p><p style="color:rgba(0, 0, 0, 0.5);margin-bottom: 5px;">' + unit_mc + '</p><p style="color:rgba(0, 0, 0, 0.5)">' + fwqx + '</p></div><div class="media-right"><img class="img1" src="./img/' + qylx + '.png" alt="小规模"></div></div></li><li><div class="col-xs-12" style="height: 9px"></div></li>');
             }
+            $(".img1").css("width", screen.availWidth * 0.14492);
         }
     },
 });
